df9dc73a3de6cbc3d7319872e44646d600c24b25
Leonam Santos <leonam.santos@deliveryhero.com>
2021-12-14 10:54:05 +0100

    Remove users

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,20 +6,16 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { environment } from 'src/environments/environment';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { AuthGuard } from './auth/auth.guard';
 import { AuthModule } from './auth/auth.module';
 import { MaterialModule } from './material/material.module';
 import { HeaderComponent } from './navigation/header/header.component';
 import { SidenavListComponent } from './navigation/sidenav-list/sidenav-list.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
-import { UiService } from './shared/ui.service';
-import { UserListComponent } from './user-list/user-list.component';
 import { WelcomeComponent } from './welcome/welcome.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    UserListComponent,
     WelcomeComponent,
     PageNotFoundComponent,
     HeaderComponent,
